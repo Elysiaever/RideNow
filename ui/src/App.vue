e968526f6ae04a94df1826eab8a1c9b89b25c225
Elysiaever <157801479+Elysiaever@users.noreply.github.com>
2025-11-24 22:14:40 +0800

    配置CORS，并进行了测试

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import axios from 'axios';
+
+function hello(){
+  axios.get('http://localhost:80/api/driver/hello').then(function (response) {
+    console.log(response);
+  });
+}
+</script>
 
 <template>
   <h1>Let's ride now!</h1>
   <p>
     This is a Blank Page...
   </p>
+  <button @click="hello">Click me</button>
 </template>
 
 <style scoped></style>

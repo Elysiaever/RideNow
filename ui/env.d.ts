2605c4650aa6eae648c50747a7fe73146574826f
Elysiaever <157801479+Elysiaever@users.noreply.github.com>
2025-11-26 23:03:37 +0800

    修改

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
+  VITE_APP_ENV: 'development' | 'test' | 'production'
+  VITE_API_BASE_URL: string
 }
 
 interface ImportMeta {
